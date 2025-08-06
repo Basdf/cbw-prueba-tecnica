673e2d30357845d4d96b8449a82163118cbe72dd
Eduardo Velasquez Velez <eduardo.velasquez@guane.com.co>
2025-08-05 21:25:58 -0500

    Refactor TaskRepository and TaskService methods to be asynchronous

--- app/domains/ports/task.py
+++ app/domains/ports/task.py
@@ -11,29 +11,29 @@ from app.domains.models.task import (
 
 class TaskRepository(ABC):
     @abstractmethod
-    def get_all(self) -> TaskResponse:
+    async def get_all(self) -> list[TaskResponse]:
         pass
 
     @abstractmethod
-    def get_by_id(self, id: str) -> TaskResponse:
+    async def get_by_id(self, id: str) -> TaskResponse:
         pass
 
     @abstractmethod
-    def get_by_status(self, status: TaskStatus) -> TaskResponse:
+    async def get_by_status(self, status: TaskStatus) -> list[TaskResponse]:
         pass
 
     @abstractmethod
-    def create(self, new_task: CreateTaskRequest) -> TaskResponse:
+    async def create(self, new_task: CreateTaskRequest) -> TaskResponse:
         pass
 
     @abstractmethod
-    def update(self, id: str, put_task: PutTaskRequest) -> TaskResponse:
+    async def update(self, id: str, put_task: PutTaskRequest) -> TaskResponse:
         pass
 
     @abstractmethod
-    def patch(self, id: str, patch_task: PatchTaskRequest) -> TaskResponse:
+    async def patch(self, id: str, patch_task: PatchTaskRequest) -> TaskResponse:
         pass
 
     @abstractmethod
-    def delete(self, id: str) -> None:
+    async def delete(self, id: str) -> None:
         pass
--- app/domains/services/task.py
+++ app/domains/services/task.py
@@ -12,23 +12,23 @@ class TaskService:
     def __init__(self, repository: TaskRepository):
         self.repository = repository
 
-    def get_all_tasks(self) -> TaskResponse:
+    async def get_all_tasks(self) -> list[TaskResponse]:
         return self.repository.get_all()
 
-    def get_task_by_id(self, id: str) -> TaskResponse:
+    async def get_task_by_id(self, id: str) -> TaskResponse:
         return self.repository.get_by_id(id)
 
-    def get_tasks_by_status(self, status: TaskStatus) -> TaskResponse:
+    async def get_tasks_by_status(self, status: TaskStatus) -> list[TaskResponse]:
         return self.repository.get_by_status(status)
 
-    def create_task(self, new_task: CreateTaskRequest) -> TaskResponse:
+    async def create_task(self, new_task: CreateTaskRequest) -> TaskResponse:
         return self.repository.create(new_task)
 
-    def update_task(self, id: str, put_task: PutTaskRequest) -> TaskResponse:
+    async def update_task(self, id: str, put_task: PutTaskRequest) -> TaskResponse:
         return self.repository.update(id, put_task)
 
-    def patch_task(self, id: str, patch_task: PatchTaskRequest) -> TaskResponse:
+    async def patch_task(self, id: str, patch_task: PatchTaskRequest) -> TaskResponse:
         return self.repository.patch(id, patch_task)
 
-    def delete_task(self, id: str) -> None:
+    async def delete_task(self, id: str) -> None:
         self.repository.delete(id)
